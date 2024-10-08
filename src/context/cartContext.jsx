b22f9ba451422883fadaf6e132ad03cf7274f87e
ruuuu <rufinka_91@mail.ru>
2024-10-08 12:06:06 +0300

    add addToCart

--- src/context/cartContext.jsx
+++ src/context/cartContext.jsx
@@ -31,6 +31,30 @@ export const CartProvider = ({ children }) => { // провайдер котры
 
 
 
+  const addToCart = (product, quantity) => { // product - {id, name, price} добавляемый товар и его колво
+
+    const newCart = [...cart]; // сделали копию массива cart(три точки кладут элементы в пустой массив и раскладывают через запятую), новый массив товаров
+    // [{id, quantity }, {}]
+    
+    const itemIndex = newCart.findIndex((cartItem) => cartItem.id === product.id);  // индекс товара которого нет в Корзине
+
+    
+    if(itemIndex >= 0) {  // если в Корзине есть товар product
+      newCart[itemIndex].quantity += quantity;
+    }
+    else{
+      newCart.push({ ...product, quantity });  // если в Корзине нет товара product, ...product вернет {id, name, price}
+    }
+
+
+    setCart(newCart); // cart= newCart
+    // localStorage.setItem('cart', JSON.stringify(cart));  // обновлем LocalStorage
+  };
+
+
+  const change
+
+
   return (
     <CartContext.Provider  value={{ cart }}>
         {children}   {/* children(др компоненты) имеют доступ к cart */}
